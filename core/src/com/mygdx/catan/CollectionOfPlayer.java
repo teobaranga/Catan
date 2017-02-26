55b59de37d8d5c217431bd41525a04f49f8f54c5
Emma Akouri <essma.akouri@mail.mcgill.ca>
2017-02-25 21:58:35 -0500

    filled in player methods, created eventDie enum, and template collectionOfPlayers

---
+++ core/src/com/mygdx/catan/CollectionOfPlayer.java
@@ -0,0 +1,20 @@
+package com.mygdx.catan;
+
+public class CollectionOfPlayer {
+	
+	public void add(Player p) {
+		
+	}
+	
+	public Player getPlayer(int index) {
+		throw new RuntimeException("getPlayer not yet implemented");
+	}
+	
+	public int size() {
+		throw new RuntimeException("size not yet implemented");
+	}
+	
+	public void remove(Player player) {
+		
+	}
+}
